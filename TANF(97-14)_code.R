5e49896e07b667a62702978245c2d764b828cf49
Ben <bengoehring1@gmail.com>
2018-01-15 15:57:31 -0500

    1/15 3:57pm

--- TANF(97-14)_code.R
+++ TANF(97-14)_code.R
@@ -130,6 +130,46 @@ props_avg_rnd <- as.data.frame(sapply(props_avg[, 3:12], round, digits = 10))
 props_avg <- cbind(props_avg[, 1:2], sapply(props_avg_rnd, re_outliers))
 props_avg_vis <- cbind(props_avg[, 1:2], props_avg_rnd)
 
+# Checks ####
+check_data <- function(data) {
+  data %>% 
+    gather(key = "category", value = "value", -STATE, -year) %>% 
+    select(STATE, category, year, value) %>%
+    unite(category, category, year, sep = "_") %>% 
+    spread(key = "category", value = "value")
+}
+
+props_excel <- check_data(props_vis) %>% 
+  write_csv("Checks/props.csv")
+avg_props_excel <- check_data(avg_props_vis) %>% 
+  write_csv("Checks/avg_props.csv")
+props_avg_excel <- check_data(props_avg_vis) %>% 
+  write_csv("Checks/props_avg.csv")
+
+# NA count checks
+count_na <- function(data) {
+  na_count <-sapply(data[, 3:12], function(y) sum(is.na(y)))
+  na_count <- data.frame(na_count)
+  
+  na_count <- aggregate(data, list(data[, 2]), function(y) sum(is.na(y))) %>% 
+    select(-STATE, -year) %>% 
+    rename(year = `Group.1`)
+}
+
+na_count_props <- count_na(props)
+na_count_avg_props <- count_na(avg_props)
+na_count_props_avg <- count_na(props_avg)
+
+wb2 <- createWorkbook()
+addWorksheet(wb2, "props")
+addWorksheet(wb2, "avg_props")
+addWorksheet(wb2, "props_avg")
+
+writeData(wb2, "props", na_count_props )
+writeData(wb2, "avg_props", na_count_avg_props )
+writeData(wb2, "props_avg", na_count_props_avg )
+
+saveWorkbook(wb2, "Checks/TANF_na_check.xlsx")
 
 # Figure 1 - Annual Mean Expenditures Stacked Bar Chart ####
 
