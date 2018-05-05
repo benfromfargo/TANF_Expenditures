1d79c68c1b00ecba6afda7e531d9753e8f338ff6
Ben <bengoehring1@gmail.com>
2018-05-05 07:47:53 -0400

    5/5 7:47

--- caseloads.R
+++ caseloads.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(readxl)
-library(extrafont)
 
 files <- list.files("Caseloads/")
 files <- str_c("Caseloads/", files)
@@ -28,12 +27,33 @@ case_raw <- case_raw %>%
   mutate(prop = value/total) %>% 
   group_by(year, category) %>% 
   mutate(avg_prop = mean(prop)) %>% 
-  mutate(avg_raw = mean(value))
+  mutate(avg_raw = mean(value)) %>% 
 
+# Average Monthly TANF Families by Family Type
 case_raw %>% 
   filter(State == "Alabama") %>% 
   ggplot(aes(year, avg_raw, fill = category)) +
-  geom_bar(stat = "identity")
+  geom_bar(stat = "identity") +
+  labs(y = "Families", title = "Average Monthly TANF Families by Family Type", 
+       fill = "Family Type") +
+  scale_fill_discrete(labels = c("Child-only", "One-parent", "Two-parents")) +
+  theme_classic() +
+  theme(axis.title.x = element_blank()) +
+  scale_y_continuous(expand = c(0,0))
+
+# Percentage of Child-Only Families in the Caseload in the Average Month
+case_raw %>% 
+  filter(category == "0.families") %>% 
+  filter(State == "Alabama") %>% 
+  ggplot(aes(year, avg_prop)) +
+  geom_line(group = 1) +
+  labs(y = "Percentage of child-only families in the caseload", 
+       title = "Percentage of Child-Only Families in the Caseload in the Average Month") +
+  scale_y_continuous(labels = scales::percent) +
+  theme_classic() +
+  theme(axis.title.x = element_blank())
+  
+  
 
   
   
