d4a869d0a490fd24e520a977df87ec80d71773c6
Ben <bengoehring1@gmail.com>
2018-09-23 11:27:42 -0400

    more post c SEs

--- test.R
+++ test.R
@@ -5,6 +5,16 @@ library(readstata13)
 
 source("TANF_clean.r")
 
+csp <- read_csv("http://ippsr.msu.edu/sites/default/files/correlatesofstatepolicyprojectv2_1.csv") %>% 
+  select(state, year, ranney4_control) %>% 
+  filter(year > 1996) %>% 
+  mutate(year = year +1) %>% 
+  filter(year < 2014) %>% 
+  mutate(year = as.character(year)) %>% 
+  filter(state != "District of Columbia") %>% 
+  arrange(year) %>% 
+  mutate(state_id = rep(state.abb, 16))
+
 test <- read.dta13("caughey_warshaw_summary.dta") %>% 
   as.tibble() %>% 
   mutate(year = as.numeric(as.character(year))) %>% 
@@ -31,12 +41,15 @@ avg_props_pdata <- avg_props_pdata %>%
 avg_props_pdata <- left_join(test, avg_props_pdata, by = c("year", "state_id"))
 anti_join(test, avg_props_pdata, by = c("year", "state_id"))
 
-p1 <- felm(ba_dif ~ african_americans + hispanics + policyeconlib_est + unemployment + 
+avg_props_pdata <- left_join(csp, avg_props_pdata, by = c("year", "state_id"))
+anti_join(csp, avg_props_pdata, by = c("year", "state_id"))
+
+p1 <- felm(ba_dif ~ african_americans + hispanics + policyeconlib_est + unemployment +
              pcpi_regional + fiscal_stability + caseload + wpr
            | STATE + year | 0 | STATE, 
            data = avg_props_pdata)
 
-p2 <- felm(ba_dif ~ african_americans + hispanics + policyeconlib_est + unemployment + 
+p2 <- felm(ba_dif ~ african_americans + hispanics + policyeconlib_est + unemployment +
             pcpi_regional + fiscal_stability + caseload + wpr + ba_dif_before
            | STATE + year | 0 | STATE, 
            data = avg_props_pdata)
@@ -44,18 +57,18 @@ p2 <- felm(ba_dif ~ african_americans + hispanics + policyeconlib_est + unemploy
 stargazer(p1, p2,
           title = "Regression Output",
           column.labels = c("Model 1", "Model 2"),
+          dep.var.labels = "Basic Assistance minus WORSSI",
           covariate.labels = c("Percent of caseload that is African American", 
-                               "Percent of caseload that is Hispanic",
-                               "Policy liberalism",
-                               "Unemployment rate", 
-                               "Per capita income (in thousands)",
-                               "Fiscal balance as a percent of spending",
-                               "Percent change in caseload",
-                               "Work participation rate",
-                               "Difference in prior year"),
-          dep.var.labels = "Basic assistance minus Work-related supports, services, and in-kind benefits",
-          add.lines = list(c("Time FEs", "Yes", "Yes"),
-                           c("State FEs", "Yes", "Yes")),
+                                "Percent of caseload that is Hispanic",
+                                "Policy liberalism",
+                                "Unemployment rate", 
+                                "Per capita income (in thousands)",
+                                "Fiscal balance as a percent of spending",
+                                "Percent change in caseload",
+                                "Work participation rate",
+                                "Difference in prior year"),
+          add.lines = list(c("Time FEs", "Yes", "Yes", "Yes", "Yes"),
+                           c("State FEs", "Yes", "Yes", "Yes", "Yes")),
           header = FALSE,
           star.cutoffs = c(.05),
           notes = "*p < 0.05; SEs clustered by state",
@@ -68,22 +81,71 @@ stargazer(p1, p2,
           type = "latex",
           out = "Figures and Tables/Table1_test.html")
 
-boom <- spread(ann_means_vis, category, value) %>% 
-  mutate(dif = ba - service)
+p3 <- felm(ba_dif ~ policyeconlib_est
+           | STATE + year | 0 | STATE, 
+           data = avg_props_pdata)
 
-boom %>% 
-  ggplot(aes(year, dif, group = 1)) +
-  geom_line() +
-  labs(title = "Basic assistance minus WORSSI",
-       subtitle = "FY 1998 - 2013",
-       x = NULL, 
-       y = NULL) +
-  scale_y_continuous(labels = scales::percent_format(1),
-                     limits = c(-.20,.40),
-                     breaks = seq(-.20, .40, .20),
-                     expand = c(0,0)) +
-  scale_x_discrete(breaks = c("1998", "2003", "2008", "2013"),
-                   labels = c("1998", "2003", "2008", "2013"))
+p4 <- felm(ba_dif ~ policyeconlib_est + african_americans
+           | STATE + year | 0 | STATE, 
+           data = avg_props_pdata)
+
+p5 <- felm(ba_dif ~ policyeconlib_est + african_americans + policyeconlib_est*african_americans
+           | STATE + year | 0 | STATE, 
+           data = avg_props_pdata)
+
+p6 <- felm(ba_dif ~ ranney4_control
+           | STATE + year | 0 | STATE, 
+           data = avg_props_pdata)
+
+p7 <- felm(ba_dif ~ ranney4_control + african_americans
+           | STATE + year | 0 | STATE, 
+           data = avg_props_pdata)
+
+p8 <- felm(ba_dif ~ ranney4_control + african_americans + ranney4_control*african_americans
+           | STATE + year | 0 | STATE, 
+           data = avg_props_pdata)
+
+stargazer(p3, p4, p5, p6, p7, p8, 
+          title = "Regression Output",
+          column.labels = NULL,
+          dep.var.labels = "Basic Assistance minus WORSSI",
+          add.lines = list(c("Time FEs", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes"),
+                           c("State FEs", "Yes", "Yes", "Yes", "Yes", "Yes", "Yes")),
+          header = FALSE,
+          star.cutoffs = c(.05),
+          notes = "*p < 0.05; SEs clustered by state",
+          notes.append = FALSE,
+          notes.align = "r",
+          model.numbers = FALSE,
+          initial.zero = FALSE,
+          column.sep.width = "1pt",
+          font.size = "small",
+          type = "latex",
+          out = "Figures and Tables/Table2_test.html")
+
+
+
+
+
+
+
+
+#boom <- spread(ann_means_vis, category, value) %>% 
+#  mutate(dif = ba - service)
+#
+#boom %>% 
+#  ggplot(aes(year, dif, group = 1)) +
+#  geom_line() +
+#  labs(title = "Basic assistance minus WORSSI",
+#       subtitle = "FY 1998 - 2013",
+#       x = NULL, 
+#       y = NULL) +
+#  scale_y_continuous(labels = scales::percent_format(1),
+#                     limits = c(-.20,.40),
+#                     breaks = seq(-.20, .40, .20),
+#                     expand = c(0,0)) +
+#  scale_x_discrete(breaks = c("1998", "2003", "2008", "2013"),
+#                   labels = c("1998", "2003", "2008", "2013"))
 
 
 
